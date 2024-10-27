0940acebb51b3c378082084c9cc736d16523ecbb
Fredrik Svensson <fs222id@student.lnu.se>
2024-10-27 16:05:15 +0100

    Add index file for handling input

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { MathQuestionGenerator } from './MathQuestionGenerator';
+import { Addition } from './operations/Addition';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const generator = new MathQuestionGenerator(new Addition());
+
+console.log('Välkommen till Matematiska Labyrinten!');
+
+function askQuestion() {
+  const { question, answer } = generator.generateQuestion();
+  rl.question(`Vad är ${question}? `, (userAnswer) => {
+    if (parseInt(userAnswer) === answer) {
+      console.log('Rätt svar! Bra jobbat!');
+    } else {
+      console.log(`Tyvärr, fel svar. Rätt svar var ${answer}.`);
+    }
+    askQuestion(); // Ställ en ny fråga
+  });
+}
+
+askQuestion();
