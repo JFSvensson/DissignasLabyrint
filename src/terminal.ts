494ca1fae796a1e12a592c65c40a70bf10cbdeb7
Fredrik Svensson <fs222id@student.lnu.se>
2024-11-24 09:35:32 +0100

    Comment out, remove in future

--- src/terminal.ts
+++ src/terminal.ts
@@ -1,33 +1,33 @@
-import { MathQuestionGenerator } from './MathQuestionGenerator';
-import { Addition } from './operations/Addition';
-import * as readline from 'readline';
+// import { QuestionGenerator } from './game/QuestionGenerator';
+// import { Addition } from './operations/Addition';
+// import * as readline from 'readline';
 
-export function initTerminalGame() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+// export function initTerminalGame() {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
 
-  const generator = new MathQuestionGenerator(new Addition());
+//   const generator = new QuestionGenerator();
 
-  console.log('Välkommen till Matematiska Labyrinten!');
+//   console.log('Välkommen till Matematiska Labyrinten!');
 
-  function askQuestion() {
-    const { question, answer } = generator.generateQuestion();
-    rl.question(`Vad är ${question}? `, (userAnswer) => {
-      if (parseInt(userAnswer) === answer) {
-        console.log('Rätt svar! Bra jobbat!');
-      } else {
-        console.log(`Tyvärr, fel svar. Rätt svar var ${answer}.`);
-      }
-      askQuestion(); // Ställ en ny fråga
-    });
-  }
+//   function askQuestion() {
+//     const { question, answer } = generator.generate();
+//     rl.question(`Vad är ${question}? `, (userAnswer) => {
+//       if (parseInt(userAnswer) === answer) {
+//         console.log('Rätt svar! Bra jobbat!');
+//       } else {
+//         console.log(`Tyvärr, fel svar. Rätt svar var ${answer}.`);
+//       }
+//       askQuestion(); // Ställ en ny fråga
+//     });
+//   }
 
-  askQuestion();
-}
+//   askQuestion();
+// }
 
-if (require.main === module) {
-  initTerminalGame();
-}
+// if (require.main === module) {
+//   initTerminalGame();
+// }
 
