9bf5ddb5571b3f4a9d45aa3bd90fdddaf3fa11d0
Fredrik Svensson <svensson.jf@gmail.com>
2026-03-26 18:44:39 +0100

    Add service worker for caching and offline support

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'dissignas-labyrint-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/bundle.js',
+  '/css/game.css',
+  '/manifest.json',
+  '/icons/icon-192.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request).then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
