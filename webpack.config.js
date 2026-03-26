a08343ee399306aa645ffcc1d7e010e3e624edeb
Fredrik Svensson <svensson.jf@gmail.com>
2026-03-26 14:30:36 +0100

    Update webpack configuration to include HtmlWebpackPlugin and CopyWebpackPlugin for improved asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/web.ts',
@@ -17,5 +19,21 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/css', to: 'css' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
   },
 };
