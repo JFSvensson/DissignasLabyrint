6287ba994e02e461c7b49907b3b3a4992ff413fe
Fredrik Svensson <svensson.jf@gmail.com>
2026-04-02 16:46:54 +0200

    Add Modulo and Power operations to the operation interface tests

--- tests/operations/index.test.ts
+++ tests/operations/index.test.ts
@@ -2,13 +2,17 @@ import { Addition } from '../../src/operations/Addition';
 import { Subtraction } from '../../src/operations/Subtraction';
 import { Multiplication } from '../../src/operations/Multiplication';
 import { Division } from '../../src/operations/Division';
+import { Modulo } from '../../src/operations/Modulo';
+import { Power } from '../../src/operations/Power';
 
 describe('Operation Interface', () => {
   const operations = [
     { name: 'Addition', op: new Addition() },
     { name: 'Subtraction', op: new Subtraction() },
     { name: 'Multiplication', op: new Multiplication() },
-    { name: 'Division', op: new Division() }
+    { name: 'Division', op: new Division() },
+    { name: 'Modulo', op: new Modulo() },
+    { name: 'Power', op: new Power() }
   ];
 
   test.each(operations)('$name should implement required methods', ({ op }) => {
