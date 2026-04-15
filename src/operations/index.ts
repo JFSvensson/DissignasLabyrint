8a2414c969e9166d5d784f48a6cd3e1aced36a2a
Fredrik Svensson <svensson.jf@gmail.com>
2026-04-15 18:20:44 +0200

    Add operations module to export mathematical operations

---
+++ src/operations/index.ts
@@ -0,0 +1,7 @@
+export { Operation } from './Operation';
+export { Addition } from './Addition';
+export { Subtraction } from './Subtraction';
+export { Multiplication } from './Multiplication';
+export { Division } from './Division';
+export { Modulo } from './Modulo';
+export { Power } from './Power';
